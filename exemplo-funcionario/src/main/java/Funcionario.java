5fe6b946cd9bc8928f61734a38deb4bb05db3a7f
RafaAmancioSoares <rafaela.soares@bandtec.com.br>
2024-08-07 13:57:53 -0300

    Exemplo inicial de classes

---
+++ exemplo-funcionario/src/main/java/Funcionario.java
@@ -0,0 +1,31 @@
+public abstract class Funcionario {
+
+    // Atributos
+    private String cpf;
+    private String nome;
+
+    // Construtor
+    public Funcionario(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+
+    // Métodos
+
+    public abstract Double calcSalario();
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    // Getter do cpf
+
+    public String getCpf() {
+        return cpf;
+    }
+}
